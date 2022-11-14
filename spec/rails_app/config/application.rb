8ffac6e535d4556da0c19e0b1e2ce39b38a7097f
kohashimoto <kohashimoto@bbtower.co.jp>
2022-11-14 19:57:45 +0900

    chore(rails_app): ログを標準出力に出し、LOG_LEVEL変数でレベル変更可能にする

--- spec/rails_app/config/application.rb
+++ spec/rails_app/config/application.rb
@@ -36,5 +36,8 @@ module Dummy
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.logger = Logger.new($stdout)
+    config.log_level = ENV['LOG_LEVEL']&.downcase&.to_sym || :error
   end
 end
