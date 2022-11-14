7b39c6ce5edafb1f746e0dc0ce6041f6f25469f0
kosay <ekr59uv25@gmail.com>
2022-11-15 01:55:50 +0900

    fix(rails_app): テスト用モデルを追加

---
+++ spec/rails_app/db/migrate/20221114164540_create_relayer_tests.rb
@@ -0,0 +1,11 @@
+class CreateRelayerTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relayer_tests do |t|
+      t.binary :event_data
+      t.string :message_id
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/rails_app/db/migrate/20221114164548_create_relayable_tests.rb
@@ -0,0 +1,11 @@
+class CreateRelayableTests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relayable_tests do |t|
+      t.binary :event_data
+      t.string :message_id
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
