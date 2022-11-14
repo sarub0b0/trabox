71bcf2359b3e033862a88fec2d7a6572cf010030
kohashimoto <kohashimoto@bbtower.co.jp>
2022-11-14 13:53:24 +0900

    test(generator): --polymorphicの設定に関するテスト追加

--- spec/generator/trabox/trabox_models_generator_spec.rb
+++ spec/generator/trabox/trabox_models_generator_spec.rb
@@ -9,31 +9,67 @@ RSpec.describe Trabox::ModelGenerator, type: :generator do
   end
 
   describe 'generated files' do
-    before do
-      run_generator %w[event --polymorphic=event]
-    end
-
     describe 'model' do
       subject { file('app/models/event.rb') }
 
-      it { is_expected.to exist }
+      context '--polymorphicが設定されているとき' do
+        before do
+          run_generator ['event', '--polymorphic=event']
+        end
 
-      it { is_expected.to contain(/include Trabox::Relay::Relayable/) }
-      it { is_expected.to contain(/belongs_to :event, polymorphic: true/) }
+        it { is_expected.to exist }
+
+        it { is_expected.to contain(/include Trabox::Relay::Relayable/) }
+        it { is_expected.to contain(/belongs_to :event, polymorphic: true/) }
+      end
+
+      context '--polymorphicが未設定のとき' do
+        before do
+          run_generator ['event']
+        end
+
+        it { is_expected.to exist }
+
+        it { is_expected.to contain(/include Trabox::Relay::Relayable/) }
+        it { is_expected.not_to contain(/belongs_to :event, polymorphic: true/) }
+      end
     end
 
     describe 'migrate' do
       subject { migration_file('db/migrate/create_events.rb') }
 
-      it { is_expected.to exist }
+      context '--polymorphicが設定されているとき' do
+        before do
+          run_generator ['event', '--polymorphic=event']
+        end
 
-      it { is_expected.to contain(/t.references :event, polymorphic: true, null: false/) }
-      it { is_expected.to contain(/t.binary :event_data/) }
-      it { is_expected.to contain(/t.string :message_id/) }
-      it { is_expected.to contain(/t.datetime :published_at/) }
+        it { is_expected.to exist }
+
+        it { is_expected.to contain(/t.references :event, polymorphic: true, null: false/) }
+        it { is_expected.to contain(/t.binary :event_data/) }
+        it { is_expected.to contain(/t.string :message_id/) }
+        it { is_expected.to contain(/t.datetime :published_at/) }
+      end
+
+      context '--polymorphicが未設定のとき' do
+        before do
+          run_generator ['event']
+        end
+
+        it { is_expected.to exist }
+
+        it { is_expected.not_to contain(/t.references :event, polymorphic: true, null: false/) }
+        it { is_expected.to contain(/t.binary :event_data/) }
+        it { is_expected.to contain(/t.string :message_id/) }
+        it { is_expected.to contain(/t.datetime :published_at/) }
+      end
     end
 
     describe 'spec' do
+      before do
+        run_generator ['event', '--polymorphic=event']
+      end
+
       subject { file('spec/models/event_spec.rb') }
 
       it { is_expected.to exist }
