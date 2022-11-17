a8d398253856d9c3f07c9c5337735ffa7bc319c7
kohashimoto <kohashimoto@bbtower.co.jp>
2022-11-17 19:28:36 +0900

    feat(subscriber): ラッパーを通してgoogleのライブラリを呼ぶよう変更

--- lib/trabox/pubsub/provider/google.rb
+++ lib/trabox/pubsub/provider/google.rb
@@ -49,6 +49,34 @@ module Trabox
         end
 
         class Subscriber
+          # @param subscription_id [String]
+          # @param opts [Hash] Google::Cloud::PubSub options
+          def initialize(subscription_id, _opts = {})
+            @pubsub = Google::Cloud::Pubsub.new
+            @subscription = @pubsub.subscription subscription_id
+            @on_error = []
+
+            Rails.logger.info "Subscription '#{subscription_id}': message ordering is #{@subscription.message_ordering?}."
+          end
+
+          def listen(_opts = {}, &callback)
+            subscriber = @subscription.listen do |received_message|
+              callback.call(received_message)
+
+              received_message.acknowledge!
+            end
+
+            @on_error.each do |&cb|
+              subscriber.on_error(&cb)
+            end
+
+            Rails.logger.info 'Listening subscrition...'
+            subscriber.start.wait!
+          end
+
+          def on_error(&callback)
+            @on_error << callback
+          end
         end
       end
     end
--- lib/trabox/pubsub/subscriber.rb
+++ lib/trabox/pubsub/subscriber.rb
@@ -1,9 +1,16 @@
-require 'google/cloud/pubsub'
+require 'trabox/pubsub/provider/google'
 
 # Google::Cloud::PubSub::Topicのラッパー
 module Trabox
   module PubSub
     class Subscriber
+      def initialize(subscription_id, opts = {})
+        @subscriber = Provider::Google::Subscriber.new subscription_id, opts
+      end
+
+      def listen(opts = {}, &callback)
+        @subscriber.listen opts, &callback
+      end
     end
   end
 end
