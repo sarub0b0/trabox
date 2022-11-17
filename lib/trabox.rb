22aaaf4e70626a3e6ddd2230f757c019b7daf233
kohashimoto <kohashimoto@bbtower.co.jp>
2022-11-17 19:29:42 +0900

    refactor(trabox): DEFAULT_SELECT_LIMITをrelayモジュールに移動

--- lib/trabox.rb
+++ lib/trabox.rb
@@ -3,7 +3,3 @@ require 'trabox/version'
 require 'trabox/relay'
 require 'trabox/pubsub'
 require 'trabox/command'
-
-module Trabox
-  DEFAULT_SELECT_LIMIT = 3
-end
--- lib/trabox/relay.rb
+++ lib/trabox/relay.rb
@@ -2,3 +2,9 @@ require 'trabox/relay/ordering_key'
 require 'trabox/relay/relayable'
 require 'trabox/relay/relayer'
 require 'trabox/relay/relayable_models'
+
+module Trabox
+  module Relay
+    DEFAULT_SELECT_LIMIT = 3
+  end
+end
