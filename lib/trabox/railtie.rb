143110df4e2a83a7beee9ba0515f18b330afd9eb
kosay <ekr59uv25@gmail.com>
2022-11-19 20:05:07 +0900

    refactor(railtie): デバッグ用の実装を削除

--- lib/trabox/railtie.rb
+++ lib/trabox/railtie.rb
@@ -1,23 +1,4 @@
 module Trabox
   class Railtie < ::Rails::Railtie
-    config.before_configuration do
-      p 'before_configuration'
-    end
-
-    config.before_initialize do
-      p 'before_initialize'
-    end
-
-    config.to_prepare do
-      p 'to_prepare'
-    end
-
-    config.before_eager_load do
-      p 'before_eager_load'
-    end
-
-    config.after_initialize do
-      p 'after_initialize'
-    end
   end
 end
