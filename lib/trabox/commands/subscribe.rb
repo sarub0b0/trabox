e671c60cd24c4d70ebe1fa9ea2e9ddab0def8db4
kosay <ekr59uv25@gmail.com>
2022-11-23 19:15:13 +0900

    feat(subscribe): コマンド引数で上書きするパラメーターがないため引数をとらないようにする

--- lib/trabox/commands/subscribe.rb
+++ lib/trabox/commands/subscribe.rb
@@ -1,12 +1,9 @@
-require_relative './subscribe/argument_parser'
 require_relative './subscribe/configuration'
 
 module Trabox
   module Command
     module Subscribe
       def self.perform
-        ArgumentParser.parse!
-
         raise unless config.valid?
 
         subscriber = config.subscriber
