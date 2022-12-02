4310f124d8618b1aef51520119e2088be2fa1a41
kohashimoto <kohashimoto@bbtower.co.jp>
2022-12-02 11:55:00 +0900

    docs(initializer): 設定値についてコメント追加

--- lib/generators/trabox/configure/templates/initializer.rb
+++ lib/generators/trabox/configure/templates/initializer.rb
@@ -2,10 +2,13 @@
 
 Trabox::Command::Relay.configure do |config|
   config.limit = 10
+
+  # ref: https://api.rubyonrails.org/classes/ActiveRecord/Locking/Pessimistic.html#method-i-lock-21
   config.lock = true
   config.interval = 5
   config.log_level = :info
 
+  # String or Proc
   ordering_key = Trabox::Publisher::Google::Cloud::PubSub::OrderingKey.new ->(event) { event.model_name.name }
 
   config.publisher = Trabox::Publisher::Google::Cloud::PubSub.new(
